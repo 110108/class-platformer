9f2755ad6e197f2cdd611a54e7f4ae664628d89b
110108 <31996737+110108@users.noreply.github.com>
2017-11-15 13:39:14 -0500

    DID WERK

--- script.js
+++ script.js
@@ -8,14 +8,15 @@ function setup() {
 	let y=100;
 	let w=200;
 	let h=20;
-	let bob=new platform(x,y,w,h);
-	let jim=new hero(0,0,500,20,10,50)
+	bob=new Platform(10,50,600,20);
+	jim=new Hero(0,0,500,20,10,50)
 	console.log("setup run");
 }
 
 function draw(){
 	background(0);
 	bob.show();
+	jim.move();
 	jim.show();
 }
 
@@ -37,19 +38,37 @@ class Platform{
 
 class Hero{
 	constructor(a,b,x,y,w,h){
-		this.a=xvelocity;
-		this.b=yvelocity;
+		this.a=a;
+		this.b=b;
 		this.x=x;
 		this.y=y;
 		this.w=w;
 		this.h=h;
 	}
 
+	move(){
+		if(keyIsDown(LEFT_ARROW)){
+			this.x-=1;
+		}
+
+		if(keyIsDown(RIGHT_ARROW)){
+					this.x+=1;
+		}
+
+		if(keyIsDown(DOWN_ARROW)){
+					this.y+=1;
+		}
+
+		if(keyIsDown(UP_ARROW)){
+					this.y-=1;
+		}
+	}
+
 	show(){
 		stroke(255);
 		strokeWeight(4);
-		fill(20,275,199);
-		ellipse(this.x, this.y this.w, this.h);
+		fill(200,150,255);
+		ellipse(this.x, this.y, this.w, this.h);
 	}
 }
 
