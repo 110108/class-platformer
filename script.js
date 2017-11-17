f7c84870cdb3a6028cb22d1c556cb37f4af9f26b
110108 <31996737+110108@users.noreply.github.com>
2017-11-17 13:39:47 -0500

    i dont even know

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let plats=[];
 let bob;
 let jim;
+let color;
 
 function setup() {
-	createCanvas(1000,600);
+	createCanvas((windowWidth-20),(windowHeight-20));
 	let x=100;
 	let y=100;
 	let w=200;
 	let h=20;
 	bob=new Platform(10,50,600,20);
-	jim=new Hero(0,0,500,20,10,50)
-	console.log("setup run");
+	jim=new Hero(0,0,500,20,random(10,25),random(25,50));
+	console.log("up up down down left right b a start");
 }
 
 function draw(){
@@ -44,31 +45,41 @@ class Hero{
 		this.y=y;
 		this.w=w;
 		this.h=h;
+		color=(random(0,255),random(0,255),random(0,255));
+
 	}
 
 	move(){
 		if(keyIsDown(LEFT_ARROW)){
-			this.x-=1;
+			this.x-=3;
 		}
 
 		if(keyIsDown(RIGHT_ARROW)){
-					this.x+=1;
+					this.x+=3;
 		}
 
 		if(keyIsDown(DOWN_ARROW)){
-					this.y+=1;
+					this.y+=3;
 		}
 
 		if(keyIsDown(UP_ARROW)){
-					this.y-=1;
+					this.y-=3;
 		}
 	}
 
 	show(){
+
 		stroke(255);
 		strokeWeight(4);
-		fill(200,150,255);
+		fill(color);
 		ellipse(this.x, this.y, this.w, this.h);
+	}
+
+	checkY(){
+		if(this.y<height){
+			score--;
+			this.y=0;
+		}
 	}
 }
 
